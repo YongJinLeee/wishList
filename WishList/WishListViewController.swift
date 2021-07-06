89d782a5b7e402f07ab58135f617632ea623f009
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-07-06 18:04:16 +0900

    Feat: WishListViewController.swift to include New Features
    Class WishListViewController의 UITableViewDataSource, UITableviewDelegate 상속 추가
    
    TableView의 cell 개수, 표현방식, 동작 함수 추가.

--- WishList/WishListViewController.swift
+++ WishList/WishListViewController.swift
@@ -7,23 +7,30 @@
 
 import UIKit
 
-class WishListViewController: UIViewController {
+class WishListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    //UITableViewDataSource
+    //TableView cell 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    // TableView 표시형태(재사용시)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+        }
 
 }

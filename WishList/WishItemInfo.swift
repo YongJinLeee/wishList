b3295d4226a008c3ad6e0700e25110bf4ffa35dd
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-07-16 21:47:38 +0900

    Refactor : struct WishItemInfo를 별도 Swift 파일로 관리

---
+++ WishList/WishItemInfo.swift
@@ -0,0 +1,23 @@
+//
+//  WishItemInfo.swift
+//  WishList
+//
+//  Created by LeeYongJin on 2021/07/16.
+//
+
+import UIKit
+
+// 각 물건의 정보 구조체.
+// struct로 관리시 정보 입출 용이
+struct WishItemInfo {
+    let name: String
+    let cost: Int
+    
+    var img: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    init(name: String, cost: Int) {
+        self.name = name
+        self.cost = cost
+    }
+}
--- WishList/WishListViewController.swift
+++ WishList/WishListViewController.swift
@@ -88,20 +88,6 @@ class ListCell: UITableViewCell {
         costLabel.text = "\(Info.cost)"
     }
 }
-// 각 물건의 정보 구조체.
-// struct로 관리시 정보 입출 용이
-struct WishItemInfo {
-    let name: String
-    let cost: Int
-    
-    var img: UIImage? {
-        return UIImage(named: "\(name).jpg")
-    }
-    init(name: String, cost: Int) {
-        self.name = name
-        self.cost = cost
-    }
-}
 
 class WishListViewModel {
     // 물건 리스트

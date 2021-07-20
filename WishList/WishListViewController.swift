0afe39f9305c16a748d5251fad72658f75efa085
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-07-21 01:18:39 +0900

    Fix : CollectionView의 Auto Layout 조정
    - UICollectionViewDelegateLayoutFlow 함수로 gridcell 간격 지정해 기기별 다른 해상도 대응

--- WishList/WishListViewController.swift
+++ WishList/WishListViewController.swift
@@ -51,7 +51,6 @@ class WishListViewController: UIViewController, UICollectionViewDataSource, UICo
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfItem
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath  ) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
             return UICollectionViewCell()
@@ -60,19 +59,25 @@ class WishListViewController: UIViewController, UICollectionViewDataSource, UICo
         cell.cellDataUpdate(Info: itemInfoSetToList)
         return cell
 }
-
 //    UICollectionViewDelegate : cell 터치 및 클릭 되었을 때의 동작은
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showDetail", sender: indexPath.item)
             
     }
-    
 //    UICollectionViewDelegateLayoutFlow : 기기 별 상이한 화면 크기에 따라 cell size 계산 및 배치
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //cell 간 횡간격
+        let itemSpacing: CGFloat = 10
+        // TextBox size
+        let textAreaHeight: CGFloat = 65
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height: CGFloat = width * (10/7) + textAreaHeight
+        
+        return CGSize(width: width, height: height)
+    }
 }
-    
     // View
-    
 class ListCell: UITableViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
